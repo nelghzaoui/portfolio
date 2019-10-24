ea71e2cb1404ee7b8dd57f24a3255717945adafb
Nasreddine <elghzaouinasreddine@hotmail.com>
2019-10-24 02:03:19 +0200

    navigate with anchor

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,7 +2,14 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  template: `
+    <navbar></navbar>
+    <home id="home"></home>
+    <about id="about"></about>
+    <skill id="skills"></skill>
+    <project id="projects"></project>
+    <contact id="contact"></contact>
+  `,
+  styles: ['']
 })
 export class AppComponent {}
