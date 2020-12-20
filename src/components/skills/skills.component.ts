f8c0bd7bc5302e3ac622db6bc7baf6e38e8b3b8d
Nasreddine <eln@outlook.be>
2020-12-20 20:13:46 +0100

    group logic in one file

--- src/components/skills/skills.component.ts
+++ src/components/skills/skills.component.ts
@@ -2,8 +2,22 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'skills',
-  templateUrl: './skills.component.html',
-  styleUrls: ['./skills.component.scss'],
+  template: `
+  <section class="page-section">
+    <div class="container">
+      <div class="skills-container" class="row col-lg-12 col-md-12 col-sm-12">
+        <skills-progress-bar class="col-lg-4 col-md-6 col-sm-6" title="Ionic" percentage="80"></skills-progress-bar>
+        <skills-progress-bar class="col-lg-4 col-md-6 col-sm-6" title="Angular" percentage="80"></skills-progress-bar>
+        <skills-progress-bar class="col-lg-4 col-md-6 col-sm-6" title="HTML / CSS" percentage="80"></skills-progress-bar>
+      </div>
+    </div>
+  </section>`,
+  styles: [`.skills-container {
+    margin: 0 auto;
+    position: relative;
+    display: flex;
+    justify-content: space-around;
+}`],
 })
 export class SkillsComponent implements OnInit {
   constructor() {}
